4fcbb1fceec3de5c7429ae63db2cc7aada075b86
sikhye1977 <sikhye1977@naver.com>
2024-04-11 04:41:05 +0900

    Librarypage 제작

--- ex_project/src/pages/LibraryPage.jsx
+++ ex_project/src/pages/LibraryPage.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import {Box, Grid,Divider} from '@mui/material';
 
 export default function LibraryPage() {
   return (
     <div>
-        <h1>Library Page</h1>
+      <Box sx={{marginLeft: '200px', marginRight: '200px', display: 'flex', flexDirection: 'column', justifyContent: 'center',backgroundColor : 'white' }}>
+        <h1>음원 차트</h1>
+        <Divider sx={{marginBottom: '10px'}} />
+        <Grid item xs={10} sm={6} md={4}>
+        <Grid container spacing={2}>
+          <Grid item xs={6} sx={{whiteSpace: 'nowrap'}}>
+            <p1>곡/앨범</p1>
+          </Grid>
+          <Grid item xs={3} sx={{whiteSpace: 'nowrap'}}>
+            <p1>아티스트</p1>
+          </Grid>
+          <Grid item xs={1} sx={{whiteSpace: 'nowrap'}}>
+            <p1>음원구매</p1>
+          </Grid>
+          <Grid item xs={1} sx={{whiteSpace: 'nowrap'}}>
+            <p1>NFT구매</p1>
+          </Grid>
+        </Grid>
+        <Divider sx={{marginTop:'10px',marginBottom: '10px'}} />
+        </Grid>
+      </Box>
     </div>
   );
 }
--- ex_project/src/pages/SigninPage.jsx
+++ ex_project/src/pages/SigninPage.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Box, Button, TextField} from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 import {useNavigate} from 'react-router-dom'
 
 export default function SigninPage() {
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [password, setPassword] = useState('');
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
   const Navigate = useNavigate();
   return (
     <div>
@@ -13,11 +23,22 @@ export default function SigninPage() {
         ></TextField>
         <TextField sx={{width : '300px', height:'40px',backgroundColor : 'white', marginBottom: '30px'}}
           label="비밀번호"
-        ></TextField>
+          type={passwordVisible ? 'text' : 'password'}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          InputProps={{
+            endAdornment: (
+              <IconButton onClick={togglePasswordVisibility}>
+                {passwordVisible ? <VisibilityOffIcon /> : <VisibilityIcon />}
+              </IconButton>
+            ),
+          }}
+        >
+        </TextField>
         <Box>
           <Button onClick={() => {Navigate('/signup')}}>회원가입</Button>
         </Box>
-        <Button>로그인</Button>
+        <Button sx={{width : '300px', height : '40px', color : 'black', backgroundColor : '#d0d0d0'}}>로그인</Button>
        </Box>
       </Box>
     </div>
